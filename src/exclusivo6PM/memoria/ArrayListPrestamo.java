a6af2b5bd11d3160c3fb3203c5691efd8e08d3e9
Carlos Gochez <cgotcha@gmail.com>
2015-06-17 19:58:56 -0600

    Mas funciones de lista enlazada

--- src/exclusivo6PM/memoria/ArrayListPrestamo.java
+++ src/exclusivo6PM/memoria/ArrayListPrestamo.java
@@ -42,6 +42,76 @@ public class ArrayListPrestamo {
      * @return True si lo pudo eliminar o no
      */
     public boolean remove(String nombre){
-        
+        if (!isEmpty()){
+            if(root.cliente.equals(nombre)){
+                root= root.next;
+                return true;
+            }
+            Prestamo mochilero= root;
+            while(mochilero.next!=null){
+                if(mochilero.next.cliente.equals(nombre)){
+                    mochilero.next= mochilero.next.next;
+                    return true;
+                }
+                mochilero= mochilero.next;
+            }
+        }
+        return false;
+    }
+    
+    /**
+     * Voy a agregar un nuevo prestamo despues del 
+     * prestamo cuyo cliente se llama igual que after
+     * @param after Cliente a buscar
+     * @param obj El prestamo Nuevo
+     */
+    public void add(String after, Prestamo obj){
+        if(obj == null) return;
+        Prestamo mochilero = root;
+        while(mochilero != null){
+            if(mochilero.cliente.equals(after)){
+                obj.next = mochilero.next;
+                mochilero.next = obj;
+                return;
+            }
+            mochilero = mochilero.next;
+        }
+    }
+    
+    /**
+     * Limpia toda la lista
+     */
+    public void clear(){
+        root = null;
+    }
+    
+    /**
+     * Retorna true si hay un prestamo con ese cliente
+     * o no
+     * @param cliente
+     * @return 
+     */
+    public boolean contains(String cliente){
+        return false;
+    }
+    
+    /**
+     * Retorna la cantidad total de prestamos en el listado
+     * @return 
+     */
+    public int size(){
+        return 0;
+    }
+    
+    /**
+     * Retorna el objeto prestamo que esta en esa posicion
+     * comenzando desde cero.
+     * Si la pos es negativa o mayor que size, se lanza
+     * IndexOutOfBoundException
+     * @param pos
+     * @return 
+     */
+    public Prestamo get(int pos){
+        return null;
     }
 }
--- src/exclusivo6PM/memoria/PrestamosTest.java
+++ src/exclusivo6PM/memoria/PrestamosTest.java
@@ -5,6 +5,8 @@
  */
 package exclusivo6PM.memoria;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Aula
@@ -12,11 +14,55 @@ package exclusivo6PM.memoria;
 public class PrestamosTest {
     public static void main(String[] args) {
         ArrayListPrestamo list = new ArrayListPrestamo();
+        /*Scanner lea = new Scanner(System.in);
+        int op;
+        
+        do{
+            System.out.println("1- Agregar prestamo");
+            System.out.println("2- Listar Prestamos");
+            System.out.println("3- Remover Prestamo");
+            System.out.println("4- Agregar Despues de");
+            System.out.println("5- Cantidad Total");
+            System.out.println("6- Limpiar Lista");
+            System.out.println("7- Conseguir Prestamo");
+            System.out.println("10-Salir");
+            System.out.print("Escoja opcion: ");
+            op = lea.nextInt();
+            
+            switch(op){
+                case 1:
+                    System.out.print("Nombre: ");
+                    String n = lea.next();
+                    System.out.print("Monto: ");
+                    double m = lea.nextDouble();
+                    list.add(new Prestamo(n, m));
+                    break;
+                case 2:
+                    list.print();
+                    break;
+                case 3:
+                    System.out.print("Nombre: ");
+                    n = lea.next();
+                    if(list.remove(n))
+                        System.out.println("Se pudo borrar");
+                    else
+                        System.out.println("No se encontro");
+                    break;
+                
+            }
+        }while(op!=10);*/
         
         list.add( new Prestamo("Alvaro", 5000));
         list.add( new Prestamo("Douglas",3000));
         list.add( new Prestamo("Alejandro", 6500));
         list.add( new Prestamo("Carlos", 2500));
+        
+        
+        list.add("Douglas", new Prestamo("Messi",9000));
+        list.add("Messi", new Prestamo("Neymar",9000));
+        list.add("Carlos", new Prestamo("Gochez",9000));
+        list.add("Bryan", new Prestamo("Eduardo",9000));
+        
         list.print();
     }
 }
